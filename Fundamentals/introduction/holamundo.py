5f8388e08f00a66d6081ad8790b28bcba5682542
MonserratRivas <catalinarivas.iccp@gmail.com>
2023-04-18 16:02:24 -0400

    subir

---
+++ Fundamentals/introduction/holamundo.py
@@ -0,0 +1,15 @@
+print("Hola, munditoo")
+# hola 
+""" hola mundo """
+x = "Hola, Python"
+print(x)
+y = 42
+print(y)
+
+a = (200)
+print(a)
+
+print("Hola Naomi, tienes covid")
+
+miami = ("Naomiiii")
+print(miami)
